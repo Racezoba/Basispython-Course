72b38bb5da0b07b128ac2c9e8942f053d443f51c
Racezoba <102217923+Racezoba@users.noreply.github.com>
2022-03-23 19:22:12 +0700

    Assignment 02
    Draw back hole by python_turtle

---
+++ black_hole.py
@@ -0,0 +1,23 @@
+import turtle
+tao = turtle.Pen()
+tao.shape('turtle')
+tao.shape("turtle")
+def black_hole():
+    for black_hole in range(200):
+        tao.fd(50)
+        tao.right(45)
+        tao.fd(50)
+        tao.right(90)
+        tao.fd(100)
+        tao.fd(50)
+        tao.left(70)
+        
+
+
+        
+black_hole()
+
+tao.penup()
+tao.fd(1000)
+
+
